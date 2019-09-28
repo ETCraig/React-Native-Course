680df4a66eaf5752af09d0d0f3267ae24b0b02aa
unknown <devethancraig@gmail.com>
2019-09-28 14:00:00 +0100

    Search Bar Styling Done

--- food-app/src/components/SearchBar.js
+++ food-app/src/components/SearchBar.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
-        <View style={styles.background}>
-            <Text>Search Screen</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                placeholder="Search"
+                style={styles.inputStyle}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: "row"
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: "center",
         marginHorizontal: 15
     }
 });

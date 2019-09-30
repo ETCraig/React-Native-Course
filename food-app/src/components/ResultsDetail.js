d632a296d9c0426ec067e83f3c71913b84a9108d
unknown <devethancraig@gmail.com>
2019-09-30 14:00:00 +0100

    ResultsDetail Created - Furthur Styling Added

---
+++ food-app/src/components/ResultsDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+const ResultsDetail = ({ result }) => {
+    return (
+        <View>
+            <Image source={{ uri: result.image_url }} style={styles.imageStyles} />
+            <Text style={styles.nameStyles}>{result.name}</Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    imageStyles: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    nameStyles: {
+        fontWeight: 'bold'
+    }
+});
+
+export default ResultsDetail;

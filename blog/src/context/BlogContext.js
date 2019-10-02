3bb2bd6f76c1d593295ba06d1592940ee7a79ad4
unknown <devethancraig@gmail.com>
2019-10-02 10:21:00 +0100

    Switched from useState to useReducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    let { type, payload } = action;
+    switch (action) {
+        case 'ADD_BLOGPOST':
+            return [
+                ...state,
+                { title: `Blog Post #${state.length + 1}` }
+            ];
+        default:
+            return state;
+    }
+}
+
 export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+        dispatch({
+            type: 'ADD_BLOGPOST'
+        });
     }
 
     return (
         <BlogContext.Provider
-            value={{ data: blogPosts, addBlogPost: addBlogPost }}
+            value={{ data: blogPosts, addBlogPost }}
         >
             {children}
         </BlogContext.Provider>

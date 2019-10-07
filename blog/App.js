f45ce41be9b759fdc7a8c697832e53da5c44322a
unknown <devethancraig@gmail.com>
2019-10-07 08:20:00 +0100

    EditScreen Component && Routing To Created

--- blog/App.js
+++ blog/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 import IndexScreen from './src/screens/IndexScreen';
 import ShowScreen from './src/screens/ShowScreen';
 import { Provider } from './src/context/BlogContext';
@@ -9,7 +10,8 @@ import { Provider } from './src/context/BlogContext';
 const navigator = createStackNavigator({
   Index: IndexScreen,
   Show: ShowScreen,
-  Create: CreateScreen
+  Create: CreateScreen,
+  Edit: EditScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {

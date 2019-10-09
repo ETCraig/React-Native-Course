f16ffd8655f480d2d02847a56624ed0848e04897
unknown <devethancraig@gmail.com>
2019-10-09 18:05:00 +0100

    MongoDB Working - Application is Using Routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+
+const authRoutes = require('../routes/authRoutes');
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = '';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB.');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to MongoDB', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi There.');

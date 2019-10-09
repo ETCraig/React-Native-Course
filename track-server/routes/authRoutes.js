fead82ea62847f51aa23d1a8152256164081d8e7
unknown <devethancraig@gmail.com>
2019-10-09 18:25:00 +0100

    User Schema Created && authRoute Now Creates New User

--- track-server/routes/authRoutes.js
+++ track-server/routes/authRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-    res.send('Post Request Hit.');
+router.post('/signup', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = new user({ email, password });
+        await user.save();
+
+        res.status(200).send('User Saved.');
+    } catch (error) {
+        res.status(422).send('Error: ', err);
+    }
 });
 
 module.exports = router;

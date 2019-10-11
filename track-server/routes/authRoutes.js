b7ba2f74531ee5d56dd8d2a766af00b12588350b
unknown <devethancraig@gmail.com>
2019-10-11 09:46:00 +0100

    Login Route Created - Password Hashing && Password Comparison Methods Created in User Model

--- track-server/routes/authRoutes.js
+++ track-server/routes/authRoutes.js
@@ -20,4 +20,26 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must Provide Email and Password.' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send({ error: 'Email Not Found.' });
+    }
+
+    try {
+        await user.comparePassword(password);
+
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+
+        res.status(200).send({ token });
+    } catch (error) {
+        res.status(404).send({ error: 'Invalid Password or Email.' });
+    }
+});
+
 module.exports = router;

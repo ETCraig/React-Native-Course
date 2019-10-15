f0233e1a88609b773bbd1ed7098c856fd09abb2e
unknown <devethancraig@gmail.com>
2019-10-15 17:14:00 +0100

    Link Added to SignUp - Reusable AuthForm Created

--- tracks/src/screens/SignUpScreen.js
+++ tracks/src/screens/SignUpScreen.js
@@ -1,42 +1,26 @@
-import React, { useContext, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import React, { useContext } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Text } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/authContext';
+import AuthForm from '../components/AuthForm';
 
 const SignUpScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Spacer>
-                <Input
-                    label="Email"
-                    value={email}
-                    onChangeText={setEmail}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                />
-            </Spacer>
-            <Spacer>
-                <Input
-                    label="Password"
-                    value={password}
-                    onChangeText={setPassword}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    secureTextEntry
-                />
-            </Spacer>
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-            <Spacer>
-                <Button title="Sign Up" onPress={() => signup({ email, password })} />
-            </Spacer>
+            <AuthForm
+                headerText="Sign Up For Tracker"
+                errorMessage={state.errorMessage}
+                buttonText="Sign Up"
+                onSubmit={signup}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>Already Have An Account? Sign In Here.</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -53,11 +37,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginBottom: 200
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
+    link: {
+        color: 'blue'
     }
 });
 

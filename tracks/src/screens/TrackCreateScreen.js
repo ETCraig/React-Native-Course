913e610b5a9e3d770ceb61da3519ad9c399d14ea
unknown <devethancraig@gmail.com>
2019-10-16 12:14:00 +0100

    Mock Location Generator && user Location Permission Check Added

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import '../_mockLocation';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+
+            await watchPositionAsync({
+                accuracy:  Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                console.log(location);
+            });
+        } catch (error) {
+            setErr(error);
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    }, []);
     return (
         <SafeAreaView>
             <Text h3>Create a Track</Text>
             <Map />
+            {err ? <Text>Please Enable Location Services.</Text> : null}
         </SafeAreaView>
     );
 }

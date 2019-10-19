676b67bf56c8f64bd606c1020142a19b17674cad
unknown <devethancraig@gmail.com>
2019-10-19 09:53:00 +0100

    Track List && Routing Done

--- tracks/src/context/trackContext.js
+++ tracks/src/context/trackContext.js
@@ -4,12 +4,21 @@ import trackerApi from '../api/tracker';
 const trackReducer = (state, action) => {
     const { type, payload } = action;
     switch (type) {
+        case 'FETCH_TRACKS':
+            return payload;
         default:
             return state;
     }
 }
 
-const fetchTasks = dispatch => () => { };
+const fetchTasks = dispatch => async () => {
+    const response = await trackerApi.get('/tracks');
+
+    dispatch({
+        type: 'FETCH_TRACKS',
+        payload: response.data
+    });
+};
 
 const createTrack = dispatch => async (name, locations) => {
     await trackerApi.post('/tracks', { name, locations });
--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Context as TrackContext } from '../context/trackContext';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+    const { state } = useContext(TrackContext);
+    const _id = navigation.getParam('_id');
+
+    const track = state.find(track => track._id === _id);
     return (
         <View>
-
+            <Text>{track.name}</Text>
         </View>
     );
 }

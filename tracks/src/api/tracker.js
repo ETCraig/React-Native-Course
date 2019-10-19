d1a207a6a27f007f9b9548e8b6e51eb8cc7f1c4a
unknown <devethancraig@gmail.com>
2019-10-19 09:10:00 +0100

    Reusable Authentication Added to api-tracker

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'http://b0958c7e.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+)
+
+export default instance;
